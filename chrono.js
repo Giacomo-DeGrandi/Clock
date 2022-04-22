210fe10e7dbee3c95cb29e123d5bef9b29375eee
giacomo-degrandi <carlo.de-grandi-giacomo@laplateforme.io>
2022-04-22 08:10:36 +0200

    chrono finished with rounds diff debut alarm

--- chrono.js
+++ chrono.js
@@ -8,26 +8,153 @@ document.addEventListener('DOMContentLoaded', function() {
     let cHou = document.querySelector("#cHou")
     let cMin = document.querySelector("#cMin")
     let cSec = document.querySelector("#cSec")
+    let cMsec = document.querySelector("#cMsec")
 
     //get my buttons
     let startStop = document.querySelector("[name=\"startStop\"]")
     let roundC = document.querySelector("[name=\"round\"]")
 
+    // get round lists
+    let roundUl = document.querySelector("#roundList")
+    let timesUl = document.querySelector("#timesList")
+
     // add events
     startStop.addEventListener( 'click', startStopClick )
     roundC.addEventListener( 'click', roundClick )
 
-    // initialise counter Start/Stop
-    let count = 0
+    // get my obj Date, necessary to manage time
+    let date = new Date(0,0,0,0,0,0,0)
+
+    // get my hrs min secs
+    let hClock = date.getHours();
+    let mClock = date.getMinutes();
+    let sClock = date.getSeconds();
+    let msClock = date.getMilliseconds();
+
+    // scope the caller to global
+    let chronoInt;
 
     // add functions
     function startStopClick(e){
-        count++;
-        if(count >= 1){
-            count = 0
-        }
+
+            // init counter
+
+            let count;
+
+            if(e.target.value === 'start'){
+
+                function myChrono(){
+                    msClock += 10
+                    if(msClock >= 1000){
+                        msClock = 0
+                        sClock ++;
+                        if(sClock === 60){
+                            sClock = 0
+                            mClock ++;
+                            if(mClock === 60){
+                                mClock = 0;
+                                hClock ++;
+                                if(hClock === 24 ){
+                                    hClock = 0
+                                    mClock = 0
+                                    sClock = 0
+                                    msClock= 0
+                                }
+                            }
+                        }
+
+                    }
+                    cHou.innerHTML = hClock
+                    cMin.innerHTML = mClock
+                    cSec.innerHTML = sClock
+
+                    date.setMilliseconds(parseInt(cMsec.innerHTML))
+
+                    let msClock2 = msClock / 10
+                    cMsec.innerHTML = msClock2.toString()
+
+                }
+
+                chronoInt = setInterval( myChrono, 10);
+
+                e.target.value = 'stop'
+
+            } else {
+
+                clearInterval(chronoInt)
+                e.target.value = 'start'
+            }
     }
+
+    // init my roundCounter
+    let roundCounter = 0;
+
+    // my array to push rounds
+    let arr = []
+    let arr2 = []
+
     function roundClick(e){
 
+        //init roundcounter
+        roundCounter ++;
+
+        let li = document.createElement('li');
+        li.setAttribute('class','p-2 display-6 w-100');
+
+        // get my hrs min secs
+        let hRound = parseInt(cHou.innerHTML)
+        let mRound = parseInt(cMin.innerHTML)
+        let sRound = parseInt(cSec.innerHTML)
+        let msRound = date.getMilliseconds();
+
+        li.innerText = '✅' + 'n.'+ roundCounter + ' ' + '🔁' + ' ' + hRound + ':' + mRound + ':' + sRound + ':' + msRound
+
+        roundUl.appendChild(li);
+
+        // get two results in separate arrays to subtract them
+        if(roundCounter % 2 === 0){
+            arr.push(hRound)
+            arr.push(mRound)
+            arr.push(sRound)
+            arr.push(msRound)
+        } else {
+            arr2.push(hRound)
+            arr2.push(mRound)
+            arr2.push(sRound)
+            arr2.push(msRound)
+        }
+
+        // init array to catch differences
+        let roundDiff = []
+
+        roundDiff = arr.map(function(item, index) {
+            // In this case item correspond to currentValue of array arr,
+            // using index to get value from array arr2
+            return item - arr2[index];
+        })
+
+        // set condition to empty arrays to be have to have jjust lasts couple of values
+        if(roundCounter % 2 === 0){
+            arr2 = []
+        } else {
+            arr = []
+        }
+
+        let li2 = document.createElement('li');
+        li2.setAttribute('class','p-2 display-6 w-100');
+
+        // first Round is always NaN
+        if(roundCounter > 1){
+
+            li2.innerText = '⏲️'
+                + Math.abs(roundDiff[0]) + ':' + Math.abs(roundDiff[1]) + ':'
+                + Math.abs(roundDiff[2]) + ':' + Math.abs(roundDiff[3])
+
+            timesUl.appendChild(li2);
+        } else  {
+
+            li2.innerText = '⏲️' + ' ' + hRound + ':' + mRound + ':' + sRound + ':' + msRound
+            timesUl.appendChild(li2);
+        }
     }
 })
--- index.php
+++ index.php
@@ -9,12 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-            crossorigin="anonymous"></script>
-    <script src="clock.js"></script>
-    <script async src="timer.js"></script>
-    <script async src="chrono.js"></script>
+    <script src="clock.js" type="application/javascript"></script>
+    <script async src="timer.js" type="application/javascript"></script>
+    <script src="chrono.js" type="application/javascript"></script>
+    <script src="alarm.js" type="application/javascript"></script>
     <title>CLOCK</title>
 </head>
 <body>
@@ -25,48 +23,48 @@
         <div class="d-flex flex-column align-items-center justify-content-center">
 
 
-            <div class="display-1 bg-light p-1 w-75 shadow-sm text-center mt-3">Current Time</div>
+            <div class="display-1 bg-eerie p-1 w-75 shadow-sm text-center mt-3">current time</div>
 
-            <div id="clock" class="display-1 py-5 bg-light fw-bold shadow-sm w-75 text-center mb-2 mt-4">
+            <div id="clock" class="display-1 py-5 bg-eerie fw-bold w-75 text-center mb-2 mt-4">
             </div>
 
 
-            <div class="display-1 bg-light p-1 w-75 shadow-sm text-center mt-3">Timer</div>
+            <div class="display-1 bg-eerie p-1 w-75 shadow-sm text-center mt-3">timer</div>
 
-                <table id="timer" class="text-center text-nowrap bg-light shadow-sm w-75 mt-2">
+                <table id="timer" class="text-center text-nowrap bg-eerie shadow-sm w-75 mt-2">
                     <tr class="d-flex flex-row display-1 p-5">
                         <th class="col border border-light shadow-sm p-2">
                             <h1>hour</h1>
                             <p id="tHou">00</p>
-                            <button type="submit" class="btn text-center btn-dark shadow-sm" name="plusH" value="+1">
+                            <button type="submit" class="btn btn-circle text-center btn-outline-light shadow-sm" name="plusH" value="+1">
                                 +
                             </button>
-                            <button type="submit" class="btn text-center btn-dark shadow-sm" name="minusH" value="-1">
+                            <button type="submit" class="btn btn-circle text-center btn-outline-light shadow-sm" name="minusH" value="-1">
                                 -
                             </button>
                         </th>
                         <th class="col border border-light shadow-sm p-2 ">
                             <h1>min</h1>
                             <p id="tMin">00</p>
-                            <button type="submit" class="btn text-center btn-dark shadow-sm" name="plusM" value="+1">
+                            <button type="submit" class="btn btn-circle text-center btn-outline-light shadow-sm" name="plusM" value="+1">
                                 +
                             </button>
-                            <button type="submit" class="btn text-center btn-dark shadow-sm" name="minusM" value="-1">
+                            <button type="submit" class="btn btn-circle text-center btn-outline-light shadow-sm" name="minusM" value="-1">
                                 -
                             </button>
                         </th>
                         <th class="col border border-light shadow-sm p-2 ">
                             <h1>sec</h1>
                             <p id="tSec">00</p>
-                            <button type="button" class="btn text-center btn-dark shadow-sm" name="plusS" value="+1">
+                            <button type="button" class="btn btn-circle text-center btn-outline-light shadow-sm" name="plusS" value="+1">
                                 +
                             </button>
-                            <button type="submit" class="btn text-center btn-dark shadow-sm" name="minusS" value="-1">
+                            <button type="submit" class="btn btn-circle text-center btn-outline-light rounded-4 shadow-sm" name="minusS" value="-1">
                                 -
                             </button>
                         </th>
                         <th class="col border border-light shadow-sm p-2 ">
-                            <div class="d-flex flex-column shadow-sm bg-light">
+                            <div class="d-flex flex-column shadow-sm bg-eerie">
                                 <button type="submit" class="btn text-center btn-outline-primary shadow-sm p-3 mt-5" name="start" value="+1">
                                     start
                                 </button>
@@ -77,8 +75,8 @@
                         </th>
                     </tr>
                     <tr class="d-flex flex-row display-1 mx-3 mb-2">
-                        <td class="d-flex flex-column display-5 w-100 mx-5 p-2">
-                            <h5 class="text-muted">Set here the time</h5>
+                        <td class="d-flex flex-column h3 w-100 mx-5 p-2">
+                            <h5 class="text-muted">set timer</h5>
                             <div class="row">
                                 <input type="number" name="hourIn" class="small p-2" placeholder=" hours"><br>
                                 <input type="number" name="minIn" class="small p-2" placeholder=" minutes"><br>
@@ -92,38 +90,62 @@
                 </table>
 
 
-            <div class="display-1 bg-light p-1 w-75 shadow-sm text-center mt-3">Chrono</div>
+            <div class="display-1 bg-eerie p-1 shadow-sm text-center mt-3">chrono</div>
 
-            <div id="chrono">
-                div
-                <table class="text-center text-nowrap bg-light shadow-sm w-75 mt-2">
-                    <tr class="d-flex flex-row display-1 p-5">
-                        <th class="col border border-light shadow-sm p-2">
+            <div id="chrono" class="w-100 d-flex flex-column align-items-center justify-content-center">
+
+                <table class="text-center text-nowrap bg-eerie shadow-sm mt-2 p-5 w-75">
+                    <tr class="display-1">
+                        <th class="border border-light shadow-sm p-2 w-25">
                             <h1>hour</h1>
                             <p id="cHou">00</p>
                         </th>
-                        <th class="col border border-light shadow-sm p-2">
+                        <th class="border border-light shadow-sm p-2 w-25">
                             <h1>min</h1>
                             <p id="cMin">00</p>
                         </th>
-                        <th class="col border border-light shadow-sm p-2">
+                        <th class="border border-light shadow-sm p-2 w-25">
                             <h1>sec</h1>
                             <p id="cSec">00</p>
                         </th>
-                    </tr>
-                    <tr class="d-flex flex-row display-1">
-                        <td class="d-flex flex-row align-items-center justify-content-center display-5 w-100 mx-5 p-2">
-                            <div class="d-flex shadow-sm bg-light">
-                                <button type="submit" class="btn text-center btn-outline-dark shadow-sm p-3" name="startStop" value="+1">
-                                    start/stop
-                                </button>
-                                <button type="submit" class="btn text-center btn-outline-dark shadow-sm p-3" name="round" value="-1">
-                                    round
-                                </button>
-                            </div>
-                        </td>
+                        <th class="border border-light shadow-sm p-2 w-25">
+                            <h1>msec</h1>
+                            <p id="cMsec">00</p>
+                        </th>
                     </tr>
                 </table>
+                <div class="d-flex flex-row display-1">
+                    <div class="d-flex flex-row align-items-center justify-content-center display-5 w-100 mx-5 p-2">
+                        <div class="d-flex shadow-sm bg-eerie">
+                            <button type="submit" class="btn text-center btn-outline-dark shadow-sm p-3" name="startStop" value="start">
+                                start/stop
+                            </button>
+                            <button type="submit" class="btn text-center btn-outline-dark shadow-sm p-3" name="round" value="-1">
+                                round
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="d-flex flex-row text-center text-nowrap bg-eerie shadow-sm mt-2 p-2">
+
+                    <div class="col w-50">
+                        <h1 class="p-2">Times</h1>
+                        <ul id="roundList" class="text-center">
+
+                        </ul>
+                    </div>
+
+                    <div class="col w-50">
+                        <h1 class="p-2">Rounds</h1>
+                        <ul id="timesList" class="text-center">
+
+                        </ul>
+                    </div>
+
+                </div>
+
             </div>
 
 
@@ -138,7 +160,7 @@
 
 <footer class=" p-5">
     <span class="p-5">
-      <a href="https://github.com/Giacomo-DeGrandi" >my github</a>
+      <a class="link-info" href="https://github.com/Giacomo-DeGrandi" >my github</a>
     </span>
 </footer>
 
